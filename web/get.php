d7d6ba035704af0122ca219df5aebad579b8c3b7
filipi <filipi@pucrs.br>
2023-07-25 10:48:29 -0300

    First tests

--- web/get.php
+++ web/get.php
@@ -2,4 +2,21 @@
 $myPATH = ini_get('include_path') . ':./include:../include:../../include';
 ini_set('include_path', $myPATH);
 include("conf.inc");
+
+
+
+echo "<PRE>";
+var_dump($origins);
+
+echo "=========================== Legacy HPS  Queue Status ===========================\n";
+$command = $dest['shell'] . " " . $dest['host'] . " " . $dest['queue_status'];
+$output = `$command`;
+echo $output;
+
+echo "============================= Head Node Disk Usage =============================\n";
+$command = $dest['shell'] . " " . $dest['host'] . " " . $dest['disk_free'];
+$output = `$command`;
+echo $output;
+
+echo "</PRE>";
 ?>
