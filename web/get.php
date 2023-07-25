2c2b0fc606ead4dd78a663bac637ed3b5193bbb2
filipi <filipi@pucrs.br>
2023-07-25 18:16:15 -0300

    minor adjustments

--- web/get.php
+++ web/get.php
@@ -3,20 +3,31 @@ $myPATH = ini_get('include_path') . ':./include:../include:../../include';
 ini_set('include_path', $myPATH);
 include("conf.inc");
 
-
-
 echo "<PRE>";
-var_dump($origins);
+//var_dump($origins);
 
-echo "=========================== Legacy HPS  Queue Status ===========================\n";
-$command = $dest['shell'] . " " . $dest['host'] . " " . $dest['queue_status'];
+echo "=============================== HPC Queue Status ===============================\n";
+$command = $dest['shell'] . " " . $dest['host2'] . " " . $dest['new_queue_status'];
 $output = `$command`;
 echo $output;
-
+echo "\n";
+echo "\n";
+echo "=========================== Legacy HPC  Queue Status ===========================\n";
+$command = $dest['shell'] . " " . $dest['host'] . " " . $dest['old_queue_status'];
+$output = `$command`;
+echo $output;
+echo "\n";
+echo "\n";
 echo "============================= Head Node Disk Usage =============================\n";
 $command = $dest['shell'] . " " . $dest['host'] . " " . $dest['disk_free'];
 $output = `$command`;
 echo $output;
-
+echo "\n";
+echo "\n";
 echo "</PRE>";
+
+$command = "ssh marfim cat /damarea/filipi/channeltracker/opencv/isolinhas_Ricardo/cropped/frame92.mini.png";
+$figura = `$command`;
+echo "<h2>in-situ Image preview</h2>";
+echo "<img alt=\"pass through image\" src=\"data:image/png;base64," . base64_encode($figura) . "\" />";
 ?>
